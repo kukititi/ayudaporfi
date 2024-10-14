81cb5fdd7349e2b7598b3d218d743feb0d543aa2
kukititi <benjamin.manriquez1@mail.udp.cl>
2024-10-14 12:20:34 -0300

    aaaaa

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', async (req, res) => {
-  const lista = await sql('SELECT * FROM products');
+app.get('/', (req, res) => {
   res.render('home', { lista });
 });
 
