2a98a1e7284b4d188f795dd6ab857fc90c848c9e
kukititi <benjamin.manriquez1@mail.udp.cl>
2024-10-22 11:45:32 -0300

    dsa

--- index.js
+++ index.js
@@ -1,37 +1,26 @@
-import path from "path";
-import { fileURLToPath } from "url";
 import { neon } from '@neondatabase/serverless';
-import express from 'express';
 import { engine } from 'express-handlebars';
-
-
+import express from 'express';
 
 const sql = neon('postgresql://piscolita_owner:qg0uBlwk4vLc@ep-withered-silence-a5uth5dy.us-east-2.aws.neon.tech/piscolita?sslmode=require'
 );
 
 const app = express();
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
-app.get('/', async (req, res) => {
-  const lista = await sql('SELECT * FROM products');
-  res.render('home', { lista });
+app.get('/', (req, res) => {
+  res.render('home');
 });
 
 
-app.get('/Accesorios', async (req, res) => {
-  const lista = await sql('SELECT * FROM products');
-res.render('accs', { lista });
+app.get('/Accesorios', (req, res) => {
+res.render('accs');
 });
 
 app.get('/Equipo', (req, res) => {
@@ -50,31 +39,6 @@ app.get('/Login', (req, res) => {
   res.render('login');
 });
 
-app.get('/prod', (req, res) => {
-  res.render('prod');
-});
 
-app.post('/products', async (req, res) => {
-  const name = req.body.name;
-  const price = req.body.price;
-  const db = req.body.db;
-  const descr = req.body.descr;
-  const categ = req.body.categ;
-  const suply = req.body.suply;
-  const image = req.body.image;
-
-  const query = 'INSERT INTO products (name, price, db, descr, categ, suply, image) VALUES ($1, $2, $3, $4, $5, $6, $7)';
-  await sql(query, [name, price, db, descr, categ, suply, image]);
-
-  res.redirect('/');
-});
-
-
-app.post('/patata', async (req, res) => {
-  const name = req.body.name;
-  const password = req.body.password;
-
-  const query = 'INSERT INTO users (name, password) VALUES ($1, $2)';
-});
 
 app.listen(3000, () => console.log('tuki'));
