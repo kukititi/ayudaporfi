c743681463f08658e8b061ed209113931a7ed2d6
kukititi <benjamin.manriquez1@mail.udp.cl>
2024-10-25 11:47:01 -0300

    dsa

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ app.post('/registrar', async (req, res) => {
 
   res.cookie(galletita, token, {maxAge: 60 * 5 * 1000});
 
-  res.redirect('/profile');
+  res.redirect('/profile', 382);
 
 });
 const authMiddleweare = (req, res, next) => {
