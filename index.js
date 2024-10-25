51d14d04f396bdfbd3526041c6fd42678581e60b
kukititi <benjamin.manriquez1@mail.udp.cl>
2024-10-25 10:13:03 -0300

    dsa

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import express from 'express';
 import path from 'path';
 import exphbs from 'express-handlebars';
 import { fileURLToPath } from "url";
+import { create } from 'express-handlebars';
+
+const hbs = create();
 
 const sql = neon('postgresql://piscolita_owner:qg0uBlwk4vLc@ep-withered-silence-a5uth5dy.us-east-2.aws.neon.tech/piscolita?sslmode=require'
 );
@@ -15,11 +18,16 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const expressHandlebars = require('express-handlebars');
+
+const hbs = expressHandlebars.create();
+app.engine('handlebars', hbs.engine);
+
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 app.use(express.static('images'));
